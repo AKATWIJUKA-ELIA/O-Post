7766e10e2926642007f36a4f98472e2c6e75987a
Elia Light <141272836+AKATWIJUKA-ELIA@users.noreply.github.com>
2025-11-17 21:21:13 +0300

    Added some Caching on Navigation

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,14 +1,18 @@
+"use client";
 import ReduxProvider from "./ReduxProvider";
 import { ConvexClientProvider } from "./ConvexClientProvider";
+import {ConvexQueryCacheProvider} from "convex-helpers/react/cache"
 import { NotificationProvider } from "@/app/NotificationContext";
 
 export default function ClientProviders({ children }: { children: React.ReactNode }) {
         return( 
                 <NotificationProvider>
         <ConvexClientProvider>
+                <ConvexQueryCacheProvider>
         <ReduxProvider>
         {children}
         </ReduxProvider>
+        </ConvexQueryCacheProvider>
         </ConvexClientProvider>
         </NotificationProvider>
         )
